2ad4b431d33437fb440df80a61e2f6d82d4ce991
Archana <archana.nair005@gmail.com>
2023-05-14 21:38:44 +0530

    test commit

---
+++ src/test/java/testjenkins/AppTest.java
@@ -0,0 +1,18 @@
+package testjenkins;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+public class AppTest {
+
+	
+
+	@Test
+	public void testMain() {
+		
+	}
+	
+
+
+}
